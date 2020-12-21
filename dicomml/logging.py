bb54990c4cebc7dbb02765ac260f52b8cb506877
Volkher Scholz <volkher.scholz@belimo.ch>
2020-12-21 09:58:58 +0000

    basic logging functionality

---
+++ dicomml/logging.py
@@ -0,0 +1,28 @@
+import logging
+
+
+DEFAULT_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stdout'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console']
+    },
+}
+
+
+def setup_logging(config: dict = DEFAULT_CONFIG, level: str = 'INFO'):
+    config['root']['level'] = level
+    logging.config.dictConfig(config)
